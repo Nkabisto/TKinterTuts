d5196206b3788704332e3cc524f10d8fa25c5d7a
Lionel Nkabi <nkabisto@gmail.com>
2023-10-09 05:40:45 +0200

    Played with a button

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
 from tkinter import *
 
-root = Tk()
+window = Tk()
+
+def myClick():
+    myLabel = Label(window ,text="Look! I clicked a Button!")
+    myLabel.pack()
+                    
 
 # Creating a label widget
-myLabel1 = Label(root,text="Hello World!").grid(row=0, column=3)
-myLabel2 = Label(root,text="My name is Lionel Nkabi").grid(row=2, column=3)
+##myLabel1 = Label(root,text="Hello World!").grid(row=0, column=3)
+##myLabel2 = Label(root,text="My name is Lionel Nkabi").grid(row=2, column=3)
 
 #shoving it onto the screen
 #myLabel1.grid(row=0, column=3)
 #myLabel2.grid(row=1, column=3)
 
-root.mainloop()
+myButton = Button(window, text="Click Me!", padx = 50, command= myClick, fg = "Purple", bg = "Yellow")
+
+myButton.pack()
+
+window.mainloop()
