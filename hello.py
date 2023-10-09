069608a3ee113716df26d8c6fe53870fbccfd22e
Lionel Nkabi <nkabisto@gmail.com>
2023-10-09 05:24:07 +0200

    Grid System

--- hello.py
+++ hello.py
@@ -3,9 +3,11 @@ from tkinter import *
 root = Tk()
 
 # Creating a label widget
-myLabel = Label(root,text="Hello World!")
+myLabel1 = Label(root,text="Hello World!").grid(row=0, column=3)
+myLabel2 = Label(root,text="My name is Lionel Nkabi").grid(row=2, column=3)
 
-#showing it onto the screen
-myLabel.pack()
+#shoving it onto the screen
+#myLabel1.grid(row=0, column=3)
+#myLabel2.grid(row=1, column=3)
 
 root.mainloop()
