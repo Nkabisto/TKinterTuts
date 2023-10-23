cff3a68ffa7cf1fccf4d7f800eb563b13754f50c
Lionel Nkabi <nkabisto@gmail.com>
2023-10-23 06:22:26 +0200

    Can read from text file and output data to screen

--- TimeAttendance.py
+++ TimeAttendance.py
@@ -1,38 +1,31 @@
 import sqlite3
-import csv
-from openpyxl import Workbook
+#from openpyxl import Workbook
 
-def populateDb(data):
-    for row in data:
-        cur.executemany("INSERT INTO timeAttendance VALUES(?,?,?,?,?)",(row,))
-        #print(row)
+def populateDb(dataFile):
+    with open(dataFile + '.txt','r') as file:
+        print('AC.No','Date','Time','Staff')
+        for line in file:
+            newLine = line.split()
+            ac_no = newLine[0]
+            cdate = newLine[1]
+            ctime = newLine[2]
+            staff = newLine[5]
+            print(ac_no,cdate,ctime,staff)
 
+
+def createTimeAndAttendanceTable():
+    cur.execute('''DROP TABLE IF EXISTS timeAttendance''')
+    cur.execute('''CREATE TABLE timeAttendance(ac_no INTEGER NOT NULL, date TEXT
+            NOT NULL, time TEXT NOT NULL, staff TEXT NOT NULL, io TEXT)''')
         
 con = sqlite3.connect('register.db')
 cur = con.cursor()
 
-cur.execute('''DROP TABLE IF EXISTS timeAttendance''')
-cur.execute('''CREATE TABLE timeAttendance(userID INTEGE R NOT NULL, date TEXT
-            NOT NULL, time TEXT NOT NULL, staff TEXT NOT NULL, io TEXT)''')
+createTimeAndAttendanceTable(); #CREATE TABLE timeAttendance
 
-dataFile = open('Time and attendance.csv')
-inputData = list(csv.reader(dataFile))
-
-populateDb(inputData)
-
-cur.execute('''SELECT * FROM timeAttendance ORDER BY userID''')
-
-users = cur.execute('''SELECT userID,staff FROM timeAttendance GROUP BY userID''')
-
-userIDs = users.fetchall()
-
-for user in userIDs:
-    cur.execute('''SELECT * FROM timeAttendance WHERE userID = ?''', (user[0],))
-
-##for user in users.fetchall():
-print(userIDs)
-
-dataFile.close()
+INPUTFILE = 'testing'
+populateDb(INPUTFILE)
 con.commit()
 con.close()
-        
+
+
