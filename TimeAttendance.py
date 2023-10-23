292c36600b63a010a90c67381b461bd5a831710f
Lionel Nkabi <nkabisto@gmail.com>
2023-10-23 06:34:58 +0200

    Can insert data into database and extract the data from database

--- TimeAttendance.py
+++ TimeAttendance.py
@@ -6,17 +6,14 @@ def populateDb(dataFile):
         print('AC.No','Date','Time','Staff')
         for line in file:
             newLine = line.split()
-            ac_no = newLine[0]
-            cdate = newLine[1]
-            ctime = newLine[2]
-            staff = newLine[5]
-            print(ac_no,cdate,ctime,staff)
+            row = [newLine[0],newLine[1],newLine[2],newLine[5]]
+            cur.executemany("INSERT INTO timeAttendance VALUES(?,?,?,?)", (row,))
 
 
 def createTimeAndAttendanceTable():
     cur.execute('''DROP TABLE IF EXISTS timeAttendance''')
     cur.execute('''CREATE TABLE timeAttendance(ac_no INTEGER NOT NULL, date TEXT
-            NOT NULL, time TEXT NOT NULL, staff TEXT NOT NULL, io TEXT)''')
+            NOT NULL, time TEXT NOT NULL, staff TEXT NOT NULL)''')
         
 con = sqlite3.connect('register.db')
 cur = con.cursor()
@@ -25,6 +22,12 @@ createTimeAndAttendanceTable(); #CREATE TABLE timeAttendance
 
 INPUTFILE = 'testing'
 populateDb(INPUTFILE)
+
+emp = cur.execute('''SELECT * FROM timeAttendance ORDER BY ac_no''')
+
+for r in emp.fetchall():
+    print(list(r))
+
 con.commit()
 con.close()
 
